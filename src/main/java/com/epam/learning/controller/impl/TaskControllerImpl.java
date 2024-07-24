549ccf6e49e5e6faca5005e2fa4c3a2573e1220b
Ziyodulla Baykhanov <ziyodulla.baykhanov@outlook.com>
2024-07-24 11:27:11 +0500

    Implement TaskControllerImpl.java

--- src/main/java/com/epam/learning/controller/impl/TaskControllerImpl.java
+++ src/main/java/com/epam/learning/controller/impl/TaskControllerImpl.java
@@ -2,6 +2,7 @@ package com.epam.learning.controller.impl;
 
 import com.epam.learning.controller.TaskController;
 import com.epam.learning.dto.TaskDto;
+import com.epam.learning.service.TaskService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,28 +13,32 @@ import java.util.List;
 @AllArgsConstructor
 public class TaskControllerImpl implements TaskController {
 
+    private final TaskService taskService;
+
     @Override
     public ResponseEntity<String> createTask(TaskDto taskDto) {
-
+        taskDto.setId(null);
+        return ResponseEntity.ok(taskService.createTask(taskDto));
     }
 
     @Override
     public ResponseEntity<TaskDto> updateTask(Integer id, TaskDto taskDto) {
-
+        taskDto.setId(id);
+        return ResponseEntity.ok(taskService.updateTask(id, taskDto));
     }
 
     @Override
     public ResponseEntity<String> deleteTask(Integer id) {
-
+        return ResponseEntity.ok(taskService.deleteTask(id));
     }
 
     @Override
     public ResponseEntity<TaskDto> getTask(Integer id) {
-
+        return ResponseEntity.ok(taskService.getTask(id));
     }
 
     @Override
     public ResponseEntity<List<TaskDto>> getAllTask() {
-
+        return ResponseEntity.ok(taskService.getAllTask());
     }
 }
