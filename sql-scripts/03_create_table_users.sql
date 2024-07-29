feae68e5019976718435ab0eccfa8861b4e0e010
Ziyodulla Baykhanov <ziyodulla.baykhanov@outlook.com>
2024-07-29 23:00:32 +0500

    Update scripts for creating and populating database tables "tasks", "users" and "tasks_users"

---
+++ sql-scripts/03_create_table_users.sql
@@ -0,0 +1,20 @@
+-- Table: public.users
+
+-- DROP TABLE IF EXISTS public.users;
+
+CREATE TABLE IF NOT EXISTS "users"
+(
+    "id" SERIAL PRIMARY KEY,
+    "user_login" VARCHAR(255),
+    "user_password" VARCHAR(255),
+    "created_datetime" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+    "deleted_datetime" TIMESTAMP WITHOUT TIME ZONE,
+    "is_deleted" BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO "users" ("user_login", "user_password")
+VALUES
+    ('+998 99 212 12 12', 'QWERTY'),
+    ('+998 90 123 45 67', 'root'),
+    ('+998 91 234 56 78', 'admin'),
+    ('+998 97 654 32 10', 'user');
